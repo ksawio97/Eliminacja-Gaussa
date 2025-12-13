94f46115c4d23da3a895b8236e02eaa3b8b3d57d
Michał Donda <01206760@pw.edu.pl>
2025-12-13 15:14:30 +0100

    Implementacja podstawienia wstecznego i dodanie testow 3, 4, 5

--- src/backsubst.c
+++ src/backsubst.c
@@ -1,11 +1,24 @@
 #include "../include/backsubst.h"
-/**
- * zwraca 0 - wsteczne podstawienie zakonczone sukcesem
- * zwraca 1 - błąd dzielenia przez 0 (element na diagonali = 0)
- * zwraca 2 - błąd nieprawidłowych rozmiarów macierzy
- */
-int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
+
+int backsubst(Matrix *x, Matrix *mat, Matrix *b) {
+    if (mat->r != mat->c || mat->r != b->r || mat->r != x->r || b->c != 1 || x->c != 1) {
+        return 2;
+    }
+
+    int i;
+    for (i = mat->r - 1; i >= 0; i--) {
+        if (mat->data[i][i] == 0) {
+            return 1;
+        }
+
+        // POPRAWKA: Inicjalizujemy x wartością z b dla tego wiersza
+        x->data[i][0] = b->data[i][0];
+
+        for (int j = i + 1; j < mat->c; j++) {
+            x->data[i][0] -= mat->data[i][j] * x->data[j][0];
+        }
+        
+        x->data[i][0] /= mat->data[i][i];
+    }
     return 0;
 }
-
-
